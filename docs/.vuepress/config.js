d1f6bb22e5602abd1011df7db7f0ecafad0ca7b9
duan <duan1104@foxmail.com>
2022-03-31 19:45:09 +0800

    🎉 init: 创建项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    title: '校园生活不完全指南🧰',
+    description: 'Just playing around',
+
+    // 主题设置
+    themeConfig: {
+        // 导航栏
+        nav: [
+        //   { text: '指南', link: '/guide/' },
+          { text: 'GitHub', link: 'https://github.com/duan-1/hnit-help' },
+        ],
+        // 侧边栏
+        sidebar: [
+            {
+                title: '开始',   // 必要的
+                collapsable: false, // 可选的，默认值是 true,
+                sidebarDepth: 1,    // 可选的，默认值是 1
+                children: [
+                    '/guide/'                    
+                ]
+            },
+            {
+                title: '校园设施',
+                collapsable: false,
+                sidebarDepth: 1,
+                children: [
+                    'guide/campus-network'
+                ]
+            }
+        ],
+        // 最后更新时间
+        lastUpdated: '最后更新于'
+    }
+
+    // 插件设置
+
+    
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+navbar: false
+home: true
+heroImage: /help.png
+heroText: 校园生活不完全指南
+tagline: 小问题包在我的身上
+actionText: 搜寻答案 →
+actionLink: /guide/
+---
+
+::: slot footer
+MIT Licensed | Copyright © 2022 [duan](https://github.com/duan-1)
+:::
